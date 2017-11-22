d33957e44d8de0c020bb099c8f12854699c02a77
ratiuseby <ratiu_sebastian1@yahoo.com>
2017-11-22 11:37:42 +0200

    Add files via upload

---
+++ QuizzApp/src/com/quiz/controllers/LobbyController.java
@@ -0,0 +1,16 @@
+package controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+
+public class LobbyController {
+
+	@FXML
+	private ComboBox<String> categories;
+	
+	@FXML
+	private Button startQuizz;
+	
+	
+}
